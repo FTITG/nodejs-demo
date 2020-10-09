266fc5f2ae1a82742a9c4fd2c76dd19e83b56da7
zhaoyue <2241633500@qq.com>
2020-10-09 08:18:16 +0800

    add

---
+++ 03-console/01-format.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+var user = {name:'王顶',
+            qq:'308542507'};
+console.log('name:%s',user.name);
+console.log('age:%d',40);
+console.log('JSON:%j',user);
+
+console.error('Error! something wrong!');
+
